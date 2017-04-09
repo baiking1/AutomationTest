8537582cc668c2b0156dd3d87689db5dc1e42a09
baiking1 <baiking1@163.com>
2017-04-09 13:22:45 +0800

    Add files via upload

---
+++ imp.py
@@ -0,0 +1,13 @@
+from time import *
+
+print(time())
+print(clock())
+print(gmtime())
+print(localtime())
+print(asctime())
+print(mktime())
+print(strftime())
+print(strptime())
+print('sleep 2 seconds')
+sleep(2)
+print(ctime())
